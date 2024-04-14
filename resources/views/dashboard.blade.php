6ff02ba1cce71d578ebf4d0dd9401ebe8aab706d
neistop <neistopx@gmail.com>
2024-04-14 21:43:31 +0600

    Вёрстка

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,18 +1,40 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
-                    PRESS FFFF
+    <div class="max-w-7xl mx-auto sm:px-6 py-10 font-bold">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-5">
+            <div class="relative shadow-sm" id="facultyInput">
+                <div class="pointer-events-none absolute inset-y-0 flex items-center pl-3">
+                    <img src="img/menu-book.svg">
                 </div>
+                <input class="w-full rounded-md pl-12 placeholder:text-gray-400" placeholder="Название факультета">
             </div>
+            <div class="relative mt-3 shadow-sm" id="directionInput">
+                <div class="pointer-events-none absolute inset-y-0 flex items-center pl-3">
+                    <img src="img/import-contacts.svg">
+                </div>
+                <input class="w-full rounded-md pl-12 placeholder:text-gray-400" placeholder="Название направления">
+            </div>
+            <div class="flex justify-between text-white" id="createBtn">
+                <button class="bg-blue-500 hover:bg-blue-700 active:bg-blue-900 py-2 px-4 rounded mt-3" id="addDirectionBtn">Добавить ещё направление</button>
+                <button class="bg-green-500 hover:bg-green-700 active:bg-green-900 py-2 px-4 rounded mt-3">Создать факультет</button>
+            </div>
+            <script>
+                $('#addDirectionBtn').click(function() {
+                    $('#createBtn').before(`
+                        <div class="flex">
+                            <div class="relative shadow-sm w-full mt-3">
+                                <div class="pointer-events-none absolute inset-y-0 flex items-center left-3">
+                                    <img src="img/import-contacts.svg">
+                                </div>
+                                <input class="w-full rounded-md pl-12 placeholder:text-gray-400" placeholder="Название направления">
+                            </div>
+                            <img class="delete-direction cursor-pointer px-2 mt-3 py-auto" src="img/delete.svg">
+                        </div>
+                    `);
+                });
+                $(document).on('click', '.delete-direction', function() {
+                    $(this).closest('.flex').remove();
+                });
+            </script>
         </div>
     </div>
 </x-app-layout>
