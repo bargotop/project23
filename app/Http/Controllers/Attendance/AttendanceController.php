ce51663345c6e6686d5bb267651ae646cca6b9c3
bargotop <bargotopx@gmail.com>
2024-05-26 22:09:00 +0600

    attendance store done

--- app/Http/Controllers/Attendance/AttendanceController.php
+++ app/Http/Controllers/Attendance/AttendanceController.php
@@ -9,54 +9,80 @@ use App\Models\Student;
 use App\Models\Schedule;
 use Carbon\Carbon;
 use Auth;
+use Illuminate\Support\Facades\Log;
 
 class AttendanceController extends Controller
 {
+    private $daysBefore = -10;
+    private $daysAfter = 5;
     public function index(Request $request, int $scheduleId)
     {
         $schedule = Schedule::with('group.students')->findOrFail($scheduleId);
         $students = $schedule->group->students;
         $dates = collect();
 
-        // Генерация дат за последние 30 дней
-        for ($i = 0; $i < 30; $i++) {
-            $dates->push(Carbon::today()->subDays($i)->format('d-m'));
+        // Получение дней недели, на которые запланированы занятия по предмету
+        $scheduledDays = Schedule::where('id', $scheduleId)->pluck('day_of_week')->unique();
+
+        // Генерация дат на основе расписания
+        for ($i = $this->daysBefore; $i <= $this->daysAfter; $i++) {
+            // если надо на все дни
+            // $dates->push(Carbon::today()->addDays($i)->format('d.m.y'));
+            $date = Carbon::today()->addDays($i);
+            if ($scheduledDays->contains(strtolower($date->format('l')))) {
+                $dates->push($date->format('d.m.y'));
+            }
         }
 
         $attendances = Attendance::where('schedule_id', $scheduleId)
             ->whereIn('date', $dates->map(function ($date) {
-                return Carbon::createFromFormat('d-m', $date)->format('Y-m-d');
+                return Carbon::createFromFormat('d.m.y', $date)->format('Y-m-d');
             }))
             ->get()
             ->groupBy(function ($attendance) {
-                return Carbon::parse($attendance->date)->format('d-m');
+                return Carbon::parse($attendance->date)->format('d.m.y');
             });
 
-        return view('teacher.attendance', compact('schedule', 'students', 'dates', 'attendances'));
+        $today = $this->getTodayDate('d.m.y');
+
+        return view('teacher.attendance', compact('schedule', 'students', 'dates', 'attendances', 'today'));
     }
 
     public function store(Request $request, $scheduleId)
     {
+        $today = $this->getTodayDate('Y-m-d');
+
         $request->validate([
             'attendances' => 'required|array',
-            'attendances.*.student_id' => 'required|exists:students,id',
-            'attendances.*.date' => 'required|date',
-            'attendances.*.is_present' => 'required|boolean',
+            'attendances.*.*.student_id' => 'required|exists:students,id',
+            'attendances.*.*.date' => 'required|date',
+            'attendances.*.*.is_present' => 'required|boolean',
         ]);
-
-        foreach ($request->attendances as $attendanceData) {
-            Attendance::updateOrCreate(
-                [
-                    'student_id' => $attendanceData['student_id'],
-                    'schedule_id' => $scheduleId,
-                    'date' => $attendanceData['date'],
-                ],
-                [
-                    'is_present' => $attendanceData['is_present'],
-                ]
-            );
+    
+        foreach ($request->attendances as $studentAttendances) {
+            foreach ($studentAttendances as $attendanceData) {
+                if ($attendanceData['date'] !== $today) {
+                    continue; // Пропускаем данные не за текущую дату
+                }
+    
+                Attendance::updateOrCreate(
+                    [
+                        'student_id' => $attendanceData['student_id'],
+                        'schedule_id' => $scheduleId,
+                        'date' => $attendanceData['date'],
+                    ],
+                    [
+                        'is_present' => $attendanceData['is_present'],
+                    ]
+                );
+            }
         }
 
         return response()->json(['success' => true]);
+    }
+
+    private function getTodayDate(string $format)
+    {
+        return Carbon::today()->format($format);
     }
 }
--- resources/views/teacher/attendance.blade.php
+++ resources/views/teacher/attendance.blade.php
@@ -1,131 +1,131 @@
 <x-app-layout>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <div class="max-w-7xl mx-auto sm:px-6">
-        <div class="shadow mt-10 sm:rounded">
-            <table class="w-full text-left">
-                <thead class="bg-gray-50">
-                    <tr>
-                        <th class="p-3">Студенты</th>
-                        <th class="p-3">01.01.24</th>
-                        <th class="p-3">02.01.24</th>
-                        <th class="p-3">03.01.24</th>
-                        <th class="p-3">04.01.24</th>
-                        <th class="p-3">05.01.24</th>
-                        <th class="p-3">06.01.24</th>
-                        <th class="p-3">07.01.24</th>
-                        <th class="p-3">08.01.24</th>
-                        <th class="p-3">09.01.24</th>
-                        <th class="p-3">10.01.24</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="border-b">
-                        <th class="p-3">
-                            Амирбок
-                        </th>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr class="border-b">
-                        <th class="p-3">
-                            Бокамир
-                        </th>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                    </tr>
-                    <tr>
-                        <th class="p-3">
-                            Бокамирбок
-                        </th>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                        <td class="p-3">
-                            <input class="border-gray-300 rounded" type="checkbox">
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        <h1>Attendance Journal for {{ $schedule->subject->name }} - {{ $schedule->group->name }}</h1>
+        <form id="attendance-form" action="{{ route('attendances.store', $schedule->id) }}" method="POST">
+            @csrf
+            <input type="hidden" name="today" value="{{ $today }}">
+            <div class="shadow mt-10 sm:rounded">
+                <table class="w-full text-left">
+                    <thead class="bg-gray-50">
+                        <tr>
+                            <th class="p-3">Студенты</th>
+                            @foreach($dates as $date)
+                                <th class="p-3 {{ $date === $today ? 'today' : 'disabled' }}">{{ $date }}</th>
+                            @endforeach
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($students as $student)
+                            <tr class="border-b">
+                                <th class="p-3">{{ $student->full_name }}</th>
+                                @foreach($dates as $date)
+                                    <td class="p-3 {{ $date === $today ? 'today' : 'disabled' }}">
+                                        <input type="hidden" name="attendances[{{ $student->id }}][{{ $date }}][student_id]" value="{{ $student->id }}">
+                                        <input type="hidden" name="attendances[{{ $student->id }}][{{ $date }}][date]" value="{{ \Carbon\Carbon::createFromFormat('d.m.y', $date)->format('Y-m-d') }}">
+                                        <input type="hidden" name="attendances[{{ $student->id }}][{{ $date }}][is_present]" value="0">
+                                        <input class="border-gray-300 rounded" type="checkbox" name="attendances[{{ $student->id }}][{{ $date }}][is_present]" value="1"
+                                            @if(isset($attendances[$date]) && $attendances[$date]->where('student_id', $student->id)->first()?->is_present)
+                                                checked
+                                            @endif
+                                            @if($date !== $today)
+                                                disabled
+                                            @endif
+                                        >
+                                    </td>
+                                @endforeach
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <button type="submit">Save Attendance</button>
+        </form>
     </div>
+    <style>
+        #div .air-datepicker {
+          width: fit-content;
+        }
+        #div .air-datepicker-body--cells {
+            grid-template-columns: repeat(7, 50px);
+            grid-auto-rows: 50px;
+        }
+
+        .today {
+            background-color: yellow;
+        }
+        .disabled {
+            background-color: lightgray;
+        }
+
+    </style>
+    <script>
+        var datePicker = new AirDatepicker('#div', {
+            onSelect({date, formattedDate, datepicker}) {
+                console.log(date, formattedDate, datepicker);
+                onSelectDate(date, formattedDate, datepicker);
+            }
+        });
+
+        function updateSubjects(forDate, scheduleData) {
+            let scheduleContainer = $('#schedule-container');
+            scheduleContainer.empty();
+            let convertedDate = forDate.split('.').reverse().join('-');
+
+            // Получаем день недели
+            let dayOfWeek = new Date(convertedDate).toLocaleDateString('ru-RU', { weekday: 'long' });
+ 
+            let headerText = `${dayOfWeek}, ${forDate}`;
+            if (scheduleData.length > 0) {
+                headerText += ` (${scheduleData.length} предмета(ов))`;
+            }
+            let scheduleHeader = $('<div class="p-3">').text(headerText);
+            scheduleContainer.prepend(scheduleHeader);
+
+            // Если нет доступных занятий, добавляем сообщение об этом
+            if (scheduleData.length === 0) {
+                scheduleContainer.append('<div class="p-3">Нет доступных занятий</div>');
+            } else {
+                // Добавляем занятия из массива scheduleData
+
+                $.each(scheduleData, function(index, item) {
+                    const subjectName = item.subject.name;
+                    const groupName = item.group.name;
+                    const startTime = item.start_time;
+                    const endTime = item.end_time;
+
+                    const scheduleItemHTML = `
+                        <div class="flex justify-between p-3 cursor-pointer hover:bg-gray-100 hover:shadow">
+                            <div>
+                                <div>${groupName}</div>
+                                <div>${subjectName}</div>
+                            </div>
+                            <div>${startTime} - ${endTime}</div>
+                        </div>
+                    `;
+                    scheduleContainer.append(scheduleItemHTML);
+                });
+            }
+        }
+
+        $('#attendance-form').on('submit', function(event) {
+            event.preventDefault();
+            $.ajax({
+                url: $(this).attr('action'),
+                method: 'POST',
+                data: $(this).serialize(),
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
+                    'Accept': 'application/json'
+                },
+                success: function(response) {
+                    location.reload();
+                },
+                error: function(xhr, status, error) {
+                    console.error('Error:', error);
+                    handleValidationErrors(xhr.responseJSON.errors);
+                }
+            });
+        });
+    </script>
 </x-app-layout>
