8db485c56aca2a6be5acd881d5c206baf0604dc7
neistop <neistopx@gmail.com>
2024-05-26 23:40:51 +0600

    Рефакторинг

--- resources/views/teacher/attendance.blade.php
+++ resources/views/teacher/attendance.blade.php
@@ -1,17 +1,16 @@
 <x-app-layout>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <div class="max-w-7xl mx-auto sm:px-6">
-        <h1>Attendance Journal for {{ $schedule->subject->name }} - {{ $schedule->group->name }}</h1>
+        <div class="mt-10">{{ $schedule->subject->name }} - {{ $schedule->group->name }}</div>
         <form id="attendance-form" action="{{ route('attendances.store', $schedule->id) }}" method="POST">
-        @csrf
-        <input type="hidden" name="today" value="{{ $today }}">
-        <div class="shadow mt-10 sm:rounded">
-            <table class="w-full text-left">
-                <thead class="bg-gray-50">
+            @csrf
+            <input type="hidden" name="today" value="{{ $today }}">
+            <table class="w-full text-left shadow mt-3 sm:rounded">
+                <thead class="bg-gray-50 border-b">
                     <tr>
                         <th class="p-3">Студенты</th>
                         @foreach($dates as $date)
-                            <th class="p-3 {{ $date['date'] === $today ? 'today' : 'disabled' }}">
+                            <th class="text-[14px] p-3 {{ $date['date'] === $today ? 'today' : 'disabled' }}">
                                 {{ $date['date'] }}<br>{{ $date['start_time'] }} - {{ $date['end_time'] }}
                             </th>
                         @endforeach
@@ -19,14 +18,14 @@
                 </thead>
                 <tbody>
                     @foreach($students as $student)
-                        <tr class="border-b">
-                            <th class="p-3">{{ $student->full_name }}</th>
+                        <tr class="border-b text-[14px]">
+                            <th class="p-2">{{ $student->full_name }}</th>
                             @foreach($dates as $date)
-                                <td class="p-3 {{ $date['date'] === $today ? 'today' : 'disabled' }}">
+                                <td class="p-2 {{ $date['date'] === $today ? 'today' : 'disabled' }}">
                                     <input type="hidden" name="attendances[{{ $student->id }}][{{ $date['date'] }}][student_id]" value="{{ $student->id }}">
                                     <input type="hidden" name="attendances[{{ $student->id }}][{{ $date['date'] }}][date]" value="{{ \Carbon\Carbon::createFromFormat('d.m.y', $date['date'])->format('Y-m-d') }}">
                                     <input type="hidden" name="attendances[{{ $student->id }}][{{ $date['date'] }}][is_present]" value="0">
-                                    <input class="border-gray-300 rounded" type="checkbox" name="attendances[{{ $student->id }}][{{ $date['date'] }}][is_present]" value="1"
+                                    <input class="w-5 h-5 border-gray-300 rounded" type="checkbox" name="attendances[{{ $student->id }}][{{ $date['date'] }}][is_present]" value="1"
                                         @if(isset($attendances[$date['date']]) && $attendances[$date['date']]->where('student_id', $student->id)->first()?->is_present)
                                             checked
                                         @endif
@@ -40,26 +39,18 @@
                     @endforeach
                 </tbody>
             </table>
-        </div>
-        <button type="submit">Save Attendance</button>
-    </form>
+            <div class="text-white float-end">
+                <button class="bg-green-500 rounded mt-3 py-2 px-4 hover:bg-green-700 active:bg-green-900" type="submit">Сохранить</button>
+            </div>
+        </form>
     </div>
     <style>
-        #div .air-datepicker {
-          width: fit-content;
-        }
-        #div .air-datepicker-body--cells {
-            grid-template-columns: repeat(7, 50px);
-            grid-auto-rows: 50px;
-        }
-
         .today {
-            background-color: yellow;
+            background-color: #F3F4F6;
         }
         .disabled {
-            background-color: lightgray;
+            background-color: #E5E7EB;
         }
-
     </style>
     <script>
         var datePicker = new AirDatepicker('#div', {
