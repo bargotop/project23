67dcd274f391daeeecbd083f5b5ca709bd0c65d6
bargotop <bargotopx@gmail.com>
2024-05-26 22:39:32 +0600

    refactoring

--- app/Http/Controllers/Attendance/AttendanceController.php
+++ app/Http/Controllers/Attendance/AttendanceController.php
@@ -13,6 +13,33 @@ use Illuminate\Support\Facades\Log;
 
 class AttendanceController extends Controller
 {
+    // $schedule = Schedule::with('group.students')->findOrFail($scheduleId);
+    // $students = $schedule->group->students;
+    // $dates = collect();
+
+    // // Получение дней недели, на которые запланированы занятия по предмету
+    // $scheduledDays = Schedule::where('id', $scheduleId)->pluck('day_of_week')->unique();
+
+    // // Генерация дат на основе расписания
+    // for ($i = $this->daysBefore; $i <= $this->daysAfter; $i++) {
+    //     // если надо на все дни
+    //     $dates->push(Carbon::today()->addDays($i)->format('d.m.y'));
+    //     // $date = Carbon::today()->addDays($i);
+    //     // if ($scheduledDays->contains(strtolower($date->format('l')))) {
+    //     //     $dates->push($date->format('d.m.y'));
+    //     // }
+    // }
+
+    // $attendances = Attendance::where('schedule_id', $scheduleId)
+    //     ->whereIn('date', $dates->map(function ($date) {
+    //         return Carbon::createFromFormat('d.m.y', $date)->format('Y-m-d');
+    //     }))
+    //     ->get()
+    //     ->groupBy(function ($attendance) {
+    //         return Carbon::parse($attendance->date)->format('d.m.y');
+    //     });
+
+    // $today = $this->getTodayDate('d.m.y');
     private $daysBefore = -10;
     private $daysAfter = 5;
     public function index(Request $request, int $scheduleId)
@@ -21,29 +48,36 @@ class AttendanceController extends Controller
         $students = $schedule->group->students;
         $dates = collect();
 
-        // Получение дней недели, на которые запланированы занятия по предмету
-        $scheduledDays = Schedule::where('id', $scheduleId)->pluck('day_of_week')->unique();
+        // Получение всех записей расписания для группы и предмета
+        $scheduledEntries = Schedule::where('group_id', $schedule->group_id)
+                                    ->where('subject_id', $schedule->subject_id)
+                                    ->get();
 
         // Генерация дат на основе расписания
         for ($i = $this->daysBefore; $i <= $this->daysAfter; $i++) {
-            // если надо на все дни
-            $dates->push(Carbon::today()->addDays($i)->format('d.m.y'));
-            // $date = Carbon::today()->addDays($i);
-            // if ($scheduledDays->contains(strtolower($date->format('l')))) {
-            //     $dates->push($date->format('d.m.y'));
-            // }
+            $date = Carbon::today()->addDays($i);
+            foreach ($scheduledEntries as $entry) {
+                if (strtolower($date->format('l')) == $entry->day_of_week) {
+                    $dates->push([
+                        'date' => $date->format('d.m.y'),
+                        'start_time' => $entry->start_time,
+                        'end_time' => $entry->end_time,
+                        'schedule_id' => $entry->id
+                    ]);
+                }
+            }
         }
 
-        $attendances = Attendance::where('schedule_id', $scheduleId)
-            ->whereIn('date', $dates->map(function ($date) {
-                return Carbon::createFromFormat('d.m.y', $date)->format('Y-m-d');
+        $attendances = Attendance::whereIn('schedule_id', $scheduledEntries->pluck('id'))
+            ->whereIn('date', $dates->map(function ($item) {
+                return Carbon::createFromFormat('d.m.y', $item['date'])->format('Y-m-d');
             }))
             ->get()
             ->groupBy(function ($attendance) {
                 return Carbon::parse($attendance->date)->format('d.m.y');
             });
 
-        $today = $this->getTodayDate('d.m.y');
+        $today = Carbon::today()->format('d.m.y');
 
         return view('teacher.attendance', compact('schedule', 'students', 'dates', 'attendances', 'today'));
     }
--- resources/views/teacher/attendance.blade.php
+++ resources/views/teacher/attendance.blade.php
@@ -3,44 +3,46 @@
     <div class="max-w-7xl mx-auto sm:px-6">
         <h1>Attendance Journal for {{ $schedule->subject->name }} - {{ $schedule->group->name }}</h1>
         <form id="attendance-form" action="{{ route('attendances.store', $schedule->id) }}" method="POST">
-            @csrf
-            <input type="hidden" name="today" value="{{ $today }}">
-            <div class="shadow mt-10 sm:rounded">
-                <table class="w-full text-left">
-                    <thead class="bg-gray-50">
-                        <tr>
-                            <th class="p-3">Студенты</th>
+        @csrf
+        <input type="hidden" name="today" value="{{ $today }}">
+        <div class="shadow mt-10 sm:rounded">
+            <table class="w-full text-left">
+                <thead class="bg-gray-50">
+                    <tr>
+                        <th class="p-3">Студенты</th>
+                        @foreach($dates as $date)
+                            <th class="p-3 {{ $date['date'] === $today ? 'today' : 'disabled' }}">
+                                {{ $date['date'] }}<br>{{ $date['start_time'] }} - {{ $date['end_time'] }}
+                            </th>
+                        @endforeach
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($students as $student)
+                        <tr class="border-b">
+                            <th class="p-3">{{ $student->full_name }}</th>
                             @foreach($dates as $date)
-                                <th class="p-3 {{ $date === $today ? 'today' : 'disabled' }}">{{ $date }}</th>
+                                <td class="p-3 {{ $date['date'] === $today ? 'today' : 'disabled' }}">
+                                    <input type="hidden" name="attendances[{{ $student->id }}][{{ $date['date'] }}][student_id]" value="{{ $student->id }}">
+                                    <input type="hidden" name="attendances[{{ $student->id }}][{{ $date['date'] }}][date]" value="{{ \Carbon\Carbon::createFromFormat('d.m.y', $date['date'])->format('Y-m-d') }}">
+                                    <input type="hidden" name="attendances[{{ $student->id }}][{{ $date['date'] }}][is_present]" value="0">
+                                    <input class="border-gray-300 rounded" type="checkbox" name="attendances[{{ $student->id }}][{{ $date['date'] }}][is_present]" value="1"
+                                        @if(isset($attendances[$date['date']]) && $attendances[$date['date']]->where('student_id', $student->id)->first()?->is_present)
+                                            checked
+                                        @endif
+                                        @if($date['date'] !== $today)
+                                            disabled
+                                        @endif
+                                    >
+                                </td>
                             @endforeach
                         </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($students as $student)
-                            <tr class="border-b">
-                                <th class="p-3">{{ $student->full_name }}</th>
-                                @foreach($dates as $date)
-                                    <td class="p-3 {{ $date === $today ? 'today' : 'disabled' }}">
-                                        <input type="hidden" name="attendances[{{ $student->id }}][{{ $date }}][student_id]" value="{{ $student->id }}">
-                                        <input type="hidden" name="attendances[{{ $student->id }}][{{ $date }}][date]" value="{{ \Carbon\Carbon::createFromFormat('d.m.y', $date)->format('Y-m-d') }}">
-                                        <input type="hidden" name="attendances[{{ $student->id }}][{{ $date }}][is_present]" value="0">
-                                        <input class="border-gray-300 rounded" type="checkbox" name="attendances[{{ $student->id }}][{{ $date }}][is_present]" value="1"
-                                            @if(isset($attendances[$date]) && $attendances[$date]->where('student_id', $student->id)->first()?->is_present)
-                                                checked
-                                            @endif
-                                            @if($date !== $today)
-                                                disabled
-                                            @endif
-                                        >
-                                    </td>
-                                @endforeach
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-            <button type="submit">Save Attendance</button>
-        </form>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <button type="submit">Save Attendance</button>
+    </form>
     </div>
     <style>
         #div .air-datepicker {
